07eb7d843e9ecd860dbf191edf1d8c1ddd64fe12
4uy0w <116808401+4uy0w@users.noreply.github.com>
2023-05-04 18:33:43 +0700

    Add files via upload

---
+++ Iloveyou.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	system("clear");
+
+	while (5 > 2){
+		textbackground(RED);
+		textcolor(YELLOW);
+		getsid(30);
+
+		cout << "I love You";
+	}
+}
